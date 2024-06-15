0ab3a531d4b9ecf7de1601d31abb03049d310250
panneerselvam <panneerselvam261@gmail.com>
2024-06-15 21:28:38 +0530

    initial commit

---
+++ src/main/java/com/mini/project/appointment/Person_storage/PersonRepository.java
@@ -0,0 +1,8 @@
+package com.mini.project.appointment.Person_storage;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person,String>{
+	
+	
+}
